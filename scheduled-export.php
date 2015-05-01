0e0bcaf19bd35a443667d19c7f21d8a4e703794a
Tim Howe <timbhowe@gmail.com>
2015-05-01 17:36:33 -0400

    Added email feild to settings page Updated comments in the plugin

--- scheduled-export.php
+++ scheduled-export.php
@@ -31,18 +31,46 @@ if (class_exists("GFForms")) {
         protected $_title = "Gravity Forms Scheduled Entries Export";
         protected $_short_title = "Scheduled Export";
 
-        public function init(){
-            parent::init();
-            add_filter("gform_submit_button", array($this, "form_submit_button"), 10, 2);
+        public function pre_init(){
+            parent::pre_init();
+            // add tasks or filters here that you want to perform during the class constructor - before WordPress has been completely initialized
         }
 
+        public function init(){
+            parent::init();
+            // add tasks or filters here that you want to perform both in the backend and frontend and for ajax requests
+        }
+
+        public function init_admin(){
+            parent::init_admin();
+            // add tasks or filters here that you want to perform only in admin
+        }
+
+        public function init_frontend(){
+            parent::init_frontend();
+            // add tasks or filters here that you want to perform only in the front end
+        }
+
+        public function init_ajax(){
+            parent::init_ajax();
+            // add tasks or filters here that you want to perform only during ajax requests
+        }
+
+		/**
+		 * Add form settings page with schedule export options.
+		 *
+		 * TODO: Add default email address
+		 *
+		 * @since    1.0.0
+		 *
+		 */
         public function form_settings_fields($form) {
             return array(
                 array(
 					'title' => "Scheduled Entries Export",
 					'description' => "The settings below will automatically export new entries and send them to the emails below based on the set time frame.",
 					'fields' => array(
-						//Activation the schedule field
+						// Activate the schedule field
 						array(
 							'label'   => "Activate the Schedule",
 							'type'    => "checkbox",
@@ -55,7 +83,7 @@ if (class_exists("GFForms")) {
 								)
 							)
 						),
-						//Set the time frame drop-down
+						// Set the time frame drop-down
 						array(
                             'label'   => "Time Frame",
                             'type'    => "select",
@@ -83,28 +111,43 @@ if (class_exists("GFForms")) {
                                     'value' => "monthly"
                                 )
                             )
-                        )
+                        ),
+                        // Set the destination email for the exported files
+                        array(
+							'type'          => "text",
+							'name'          => "email",
+							'label'         => "Email Address",
+							'class'         => "medium",
+							'tooltip'       => "Enter a comma separated list of emails you would like to receive the exported entries file."
+					   )
                     )
                 )
             );
-        }
+        } //END form_settings_fields();
 
     }
     new GFScheduledExport();
-}
 
-// Add time frame options to the cron schedule - https://codex.wordpress.org/Function_Reference/wp_get_schedules
-add_filter( 'cron_schedules', 'scheduled_export_cron_add_times' );
-function scheduled_export_cron_add_times( $schedules ) {
- 	// Adds once weekly to the existing schedules.
- 	$schedules['weekly'] = array(
- 		'interval' => 604800,
- 		'display' => __( "Weekly" )
- 	);
- 	// Add monthly
- 	$schedules['monthly'] = array(
- 		'interval' => 2592000,
- 		'display' => __( "Monthly - Every 30 Days" )
- 	);
- 	return $schedules;
+	/**
+	 * Add time frame options to the cron schedule
+	 *
+	 * TODO: Check if this should be in the class and if show where it should init?
+	 *
+	 * @reference https://codex.wordpress.org/Function_Reference/wp_get_schedules
+	 * @since    1.0.0
+	 */
+	add_filter( 'cron_schedules', 'scheduled_export_cron_add_times' );
+	function scheduled_export_cron_add_times( $schedules ) {
+	 	// Adds once weekly to the existing schedules.
+	 	$schedules['weekly'] = array(
+	 		'interval' => 604800,
+	 		'display' => __( "Weekly" )
+	 	);
+	 	// Add monthly
+	 	$schedules['monthly'] = array(
+	 		'interval' => 2592000,
+	 		'display' => __( "Monthly - Every 30 Days" )
+	 	);
+	 	return $schedules;
+	}
 }
